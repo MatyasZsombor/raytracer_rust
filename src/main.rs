432a3a210390b76f40c12f8916d429219d11ae55
Mátyás Zsombor <z.matyas@students.htl-leonding.ac.at>
2024-07-05 21:13:08 +0200

    Fixed a bug in vector addition, implemented rays, cameras and backgrounds

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,63 @@
-use std::fmt::format;
+mod vec3;
+mod color;
+mod ray;
+
 use std::fs;
-use std::fs::File;
+use crate::color::*;
+use colored::{Colorize};
+use crate::vec3::Vec3;
+use crate::ray::Ray;
+
+
+fn ray_color(ray: &Ray) -> Color
+{
+    let alpha = 0.5 * (ray.direction().normalize().y() + 1.0);
+    (1.0 - alpha) * Color::new(1.0,1.0,1.0) + alpha * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main()
 {
-    let image_width = 256;
-    let image_height = 256;
+    let image_width = 800;
+    let aspect_ratio = 16.0 / 9.0;
+
+    let mut image_height = (image_width as f32 / aspect_ratio) as i32;
+    image_height = if image_height < 1 { 1 } else { image_height} ;
+
+    let focal = 1.0;
+    let viewport_height : f32 = 2.0;
+    let viewport_width = viewport_height * (image_width as f32 / image_height as f32);
+    let camera_pos = Vec3::new_zero();
+
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    let pixel_delta_u = viewport_u / image_width as f32;
+    let pixel_delta_v = viewport_v / image_height as f32;
+
+    let viewport00 = camera_pos - Vec3::new(0.0,0.0,focal) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00 = viewport00 + 0.5 * (pixel_delta_u + pixel_delta_v);
+
 
     let mut string: String = "".to_string();
 
     string.push_str(&format!("P3\n{image_width} {image_height}\n{}\n", 255));
 
-
     for j in 0..image_height {
+        println!("{} {}", "\rScan lines remaining ".red(), (image_height - j).to_string().red());
+
         for i in 0..image_width
         {
-            let r = i as f32 / (image_width - 1) as f32;
-            let g = j as f32 / (image_height - 1) as f32;
-            let b = 0.0;
+            let pixel_center = pixel00 + (i as f32 * pixel_delta_u) + (j as f32 * pixel_delta_v);
+            let ray = Ray::new(camera_pos, pixel_center - camera_pos);
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            string.push_str(&format!("{ir} {ig} {ib}\n"));
+            let pixel_color = ray_color(&ray);
+            string.push_str(&write_color(&pixel_color));
         }
+        print!("\x1B[2J\x1B[1;1H");
     }
+    println!("{}", "Done".green());
+
 
     fs::write("helloworld.ppm", string).unwrap();
+
 }
--- src/ray.rs
+++ src/ray.rs
@@ -1,8 +1,31 @@
 use crate::vec3::Vec3;
 
 #[derive(Copy, Clone)]
-struct Ray
+pub struct Ray
 {
     origin: Vec3,
     direction: Vec3
+}
+
+impl Ray
+{
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray
+    {
+        Ray {origin, direction}
+    }
+
+    pub fn origin(&self) -> Vec3
+    {
+        self.origin
+    }
+
+    pub fn direction(&self) -> Vec3
+    {
+        self.direction
+    }
+
+    pub fn at(self, t: f32) -> Vec3
+    {
+        return self.origin + t * self.direction;
+    }
 }
