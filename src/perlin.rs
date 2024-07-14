a85c04b235105b9c9fd4d31ba21c3296b05060b2
Mátyás Zsombor <z.matyas@students.htl-leonding.ac.at>
2024-07-14 22:59:24 +0200

    Implemented Ken Perlin's trick.

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,25 +1,30 @@
 use rand::{Rng};
 use crate::vec3::Vec3;
-fn generate_random_floats(n: usize) -> Vec<f32>
+fn generate_random_vecs(n: usize) -> Vec<Vec3>
 {
     let mut rng = rand::thread_rng();
-    let mut rand_floats = Vec::with_capacity(n);
+    let mut rand_vecs = Vec::with_capacity(n);
     for _ in 0..n {
-        rand_floats.push(rng.gen::<f32>());
+        rand_vecs.push(Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0)).normalize());
     }
-    rand_floats
+    rand_vecs
 }
 
-fn trilinear_interpolation(c: [[[f32; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+fn trilinear_interpolation(c: [[[Vec3; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+    let uu = u * u * (3.0 - 2.0 * u);
+    let vv = v * v * (3.0 - 2.0 * v);
+    let ww = w * w * (3.0 - 2.0 * w);
+
     let mut acc = 0.0;
 
     for i in 0..2 {
         for j in 0..2 {
             for k in 0..2 {
-                acc += (i as f32 * u + (1.0 - i as f32)*(1.0 - u))
-                    *  (j as f32 * v + (1.0 - j as f32)*(1.0 - v))
-                    *  (k as f32 * w + (1.0 - k as f32)*(1.0 - w))
-                    *  c[i as usize][j as usize][k as usize];
+                let weight = Vec3::new(u - i as f32, v - j as f32, w - k as f32);
+                acc += (i as f32 * uu + (1.0 - i as f32)*(1.0 - uu))
+                    *  (j as f32 * vv + (1.0 - j as f32)*(1.0 - vv))
+                    *  (k as f32 * ww + (1.0 - k as f32)*(1.0 - ww))
+                    *  c[i as usize][j as usize][k as usize].dot(weight);
             }
         }
     }
@@ -52,7 +57,7 @@ fn permute(p: &mut [usize], n: usize)
 #[derive(Clone)]
 pub struct Perlin
 {
-    random_floats: Vec<f32>,
+    random_vectors: Vec<Vec3>,
     perm_x: Vec<usize>,
     perm_y: Vec<usize>,
     perm_z: Vec<usize>,
@@ -64,7 +69,7 @@ impl Perlin
     {
         Perlin
         {
-            random_floats: generate_random_floats(256),
+            random_vectors: generate_random_vecs(256),
             perm_x: perlin_generate_perm(256),
             perm_y: perlin_generate_perm(256),
             perm_z: perlin_generate_perm(256),
@@ -73,24 +78,20 @@ impl Perlin
 
     pub fn noise(&self, point: &Vec3) -> f32
     {
-        let mut u = point.x() - point.x().floor();
-        let mut v = point.y() - point.y().floor();
-        let mut w = point.z() - point.z().floor();
-
-        u = u * u * (3.0-2.0 * u);
-        v = v * v * (3.0-2.0 * v);
-        w = w * w * (3.0-2.0 * w);
+        let u = point.x() - point.x().floor();
+        let v = point.y() - point.y().floor();
+        let w = point.z() - point.z().floor();
 
         let i = point.x().floor() as i32;
         let j = point.y().floor() as i32;
         let k = point.z().floor() as i32;
 
-        let mut c = [[[0.0; 2]; 2]; 2];
+        let mut c = [[[Vec3::new_zero(); 2]; 2]; 2];
 
         for di in 0..2 {
             for dj in 0..2 {
                 for dk in 0..2 {
-                    c[di][dj][dk] = self.random_floats[
+                    c[di][dj][dk] = self.random_vectors[
                         self.perm_x[((i + di as i32) & 255) as usize] ^
                         self.perm_y[((j + dj as i32) & 255) as usize] ^
                         self.perm_z[((k + dk as i32) & 255) as usize]];
